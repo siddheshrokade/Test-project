d723186478fbd5623f6c6738f31ab22978d23aac
unknown <s.s.rokade26@gmail.com>
2024-11-10 16:38:53 +0530

    project added

---
+++ simple_pytest_project/src/main.py
@@ -0,0 +1,10 @@
+# src/main.py
+
+from math_operations import add, subtract
+
+def main():
+    print(f"2 + 3 = {add(2, 3)}")
+    print(f"5 - 3 = {subtract(5, 3)}")
+
+if __name__ == "__main__":
+    main()
---
+++ simple_pytest_project/src/math_operations.py
@@ -0,0 +1,7 @@
+# src/math_operations.py
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
